96ae2b6cc0c1419928089a1774740497702e15ff
fischermatte <fischermatte@posteo.net>
2023-10-24 21:37:25 +0200

    fix: issue with selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   imports: [RouterModule],
-  selector: 'nx-angular-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
